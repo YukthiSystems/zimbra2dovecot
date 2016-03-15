44e9445fc86f819dd8a04a172091d1df7ff58d27
Avinash Sultanpur <avinash.s@yukthi.com>
2016-03-15 10:56:27 +0530

    Prefetch metadata to speedup conversion
    Get the metadata of all files so as to avoid traversing the tar file
    repeatedly. This speeds up the conversion by many folds.

--- zimbra2dovecot.py
+++ zimbra2dovecot.py
@@ -28,9 +28,12 @@ def get_msgflags(metadata):
     return ''.join(flags)
 
 
-def get_metadata(tf, mailfile):
-    """Get the metadata for a given member from the tarfile"""
-    return loads(tf.extractfile(mailfile + '.meta').read().decode())
+def get_metadata(tf):
+    """Return a list of metadata for all mails"""
+    return {k:v for k,v in
+            map(lambda m: (m['name'].rstrip('.meta'), loads(tf.extractfile(m['name']).read().decode())),
+                filter(lambda m: m['name'].rfind('.meta', -5) >= 0,
+                       [m.get_info() for m in tf.getmembers()]))}
 
 
 def get_mails(tf):
@@ -38,10 +41,10 @@ def get_mails(tf):
                   [m.get_info() for m in tf.getmembers()])
 
 
-def store_mail(tf, member, maildir):
+def store_mail(tf, member, maildir, metadata):
     # Retrieve the message
     msg = MaildirMessage(tf.extractfile(member['name']))
-    msg.set_flags(get_msgflags(get_metadata(tf, member['name'])))
+    msg.set_flags(get_msgflags(metadata[member['name']]))
     folder = path.dirname(member['name'])
     md = Maildir(maildir)
     if not folder == 'Inbox':
@@ -53,8 +56,8 @@ if __name__ == '__main__':
     if len(sys.argv) < 3:
         print("Usage: {} /path/to/zmmailbox.tgz /path/to/dovecot/Maildir")
         exit(1)
-
     with TarFile.gzopen(sys.argv[1]) as tf:
+        metadata = get_metadata(tf)
         for m in get_mails(tf):
             print("{}".format(m['name'][:20]))
-            store_mail(tf, m, sys.argv[2])
+            store_mail(tf, m, sys.argv[2], metadata)
