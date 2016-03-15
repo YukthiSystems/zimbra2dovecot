676b581f795325f4a4768a5432cf10a9d5b16622
Avinash Sultanpur <avinash.s@yukthi.com>
2016-03-15 12:01:23 +0530

    Add comments and update docstrings

--- zimbra2dovecot.py
+++ zimbra2dovecot.py
@@ -34,22 +34,31 @@ def get_metadata(tf):
 
     """
     return {k:v for k,v in
+            # Build a list of tuples with (mail, metadata)
             map(lambda m: (m['name'].rstrip('.meta'),
+                           # Load the JSON inside the metadata files
                            loads(tf.extractfile(m['name']).read().decode())),
+                # Keep only the metadata
                 filter(lambda m: m['name'].rfind('.meta', -5) >= 0,
+                       # Build a list with mails and their metadata
                        [m.get_info() for m in tf.getmembers()]))}
 
 
 def get_mails(tf):
+    """Return a list of dicts with information from all mails in the
+    archive
+
+    """
     return filter(lambda m: m['name'].rfind('.eml', -4) >= 0,
                   [m.get_info() for m in tf.getmembers()])
 
 
-def store_mail(tf, member, maildir, metadata):
+def store_mail(tf, mail, maildir, metadata):
+    """Store the given mail inside the maildir with its metadata"""
     # Retrieve the message
-    msg = MaildirMessage(tf.extractfile(member['name']))
-    msg.set_flags(get_msgflags(metadata[member['name']]))
-    folder = path.dirname(member['name'])
+    msg = MaildirMessage(tf.extractfile(mail['name']))
+    msg.set_flags(get_msgflags(metadata[mail['name']]))
+    folder = path.dirname(mail['name'])
     md = Maildir(maildir)
     if not folder == 'Inbox':
         md = Maildir(path.join(maildir, '.' + folder), factory=None)
